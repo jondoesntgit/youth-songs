39bfbb8a5fc188d2df6382ded0d9141082325b11
jondoesntgit <jonathan.m.wheeler@gmail.com>
2017-07-23 18:33:19 -0700

    Everything seems to be in the places that it needs to be now

--- _plugins/Checked.rb
+++ _plugins/Checked.rb
@@ -1,8 +1,27 @@
 module Jekyll
   module Checked
-    def checked(text)
+    def title_slide(text)
       @text = text
-      return "foo #{@text}"
+      "<section data-menu-title=\"#{@text}\">
+        #{@text}
+      </section>"
+    end
+    def songify(lyrics, progression)
+      content =  lyrics.gsub(/\n/, "<br/>") 
+      content =  content.gsub(/(<br\/>){2,}/,"</section><section>") 
+      return "<section>" + content + "</section>" 
+   
+      # Populate an array with paragraphs 
+   
+      # Detect chorus 
+   
+      # Interleave chorus between all following verses 
+   
+      # Join paragraphs with <section> 
+   
+      # Return the HTML verse 
+   
+      return content 
     end
   end
 end
